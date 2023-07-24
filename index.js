eed25e994836c6ea88b4ae99df9d5073c3a92f7a
gsr142 <gsr142@gmail.com>
2023-07-24 12:43:44 -0700

    add questions

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Please enter your name',
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: 'Enter your location',
+            name: 'location'
+        },
+        {
+            type: 'input',
+            message: 'Enter a summary about yourself',
+            name: 'bio'
+        },
+        {
+            type: 'input',
+            message: 'Enter your LinkedIn URL',
+            name: 'linkedin'
+        },
+        {
+            type: 'input',
+            message: 'Enter your GitHub URL',
+            name: 'github'
+        },
+    ]);
